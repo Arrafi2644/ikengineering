4006355470b6b526cf9984f39d8a77b73b037eec
Arrafi2644 <raffian2644@gmail.com>
2026-04-06 14:46:53 +0600

    Add hero functionalities

---
+++ src/controllers/hero.controller.ts
@@ -0,0 +1,86 @@
+import httpStatus from 'http-status-codes';
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from '@/utils/catchAsync';
+import AppError from '@/errorHelpers/appError';
+import { sendResponse } from '@/utils/sendResponse';
+import { cloudinaryUpload, deleteImageFromCloudinary } from '@/config/cloudinary.config';
+import { HeroServices } from '@/services/hero.service';
+import { Hero } from '@/models/hero.model';
+import { IHero } from '@/interfaces/Hero.interface';
+
+const createHero = catchAsync(async (req: Request, res: Response) => {
+
+    const payload: IHero = {
+        ...req.body,
+        images: (req.files as Express.Multer.File[]).map(file => file.path)
+    }
+    const result = await HeroServices.createHero(payload);
+
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: 'Hero created successfully',
+        data: result
+    });
+});
+
+const updateHero = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const heroId = req.params.id as string;
+        const payload = req.body;
+
+        // Find existing product
+        const existingProduct = await Hero.findById(heroId);
+
+        if (!existingProduct) {
+            throw new AppError(httpStatus.NOT_FOUND, "Hero not found");
+        }
+
+        // If new images uploaded
+        if (req.files && Array.isArray(req.files)) {
+            const newImages = (req.files as Express.Multer.File[]).map(
+                (file) => file.path
+            );
+
+            // delete old images
+            if (existingProduct.images?.length && newImages.length < 0) {
+                await Promise.all(
+                    existingProduct.images.map((img) =>
+                        deleteImageFromCloudinary(img)
+                    )
+                );
+            }
+
+            payload.images = newImages;
+        }
+
+        const product = await HeroServices.updateHero(heroId, payload);
+
+        sendResponse(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: "Hero Updated Successfully",
+            data: product,
+        });
+    }
+);
+
+const getSingleHero = catchAsync(async (req: Request, res: Response) => {
+
+    const heroId = req.params.id as string;
+    const result = await HeroServices.getSingleHero(heroId);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Hero retrieved successfully',
+        data: result
+    });
+});
+
+
+export const HeroControllers = {
+    createHero,
+    getSingleHero,
+    updateHero
+}
