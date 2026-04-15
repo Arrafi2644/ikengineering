d964c06686c0fa9128db4f6926850f2150eaecef
Arrafi2644 <raffian2644@gmail.com>
2026-04-15 10:39:56 +0600

    Update seo

--- src/validations/seo.validation.ts
+++ src/validations/seo.validation.ts
@@ -6,44 +6,18 @@ export const createSEOZodSchema = z.object({
   }).min(1, "Page Path cannot be empty"),
   metaTitle: z.string({
     required_error: "Meta Title is required",
-  }).max(60, "Meta Title should be max 100 characters"),
+  }),
   metaDescription: z.string({
     required_error: "Meta Description is required",
-  }).max(160, "Meta Description should be max 200 characters"),
+  }),
   metaKeywords: z.string().optional(),
-  // canonicalURL: z.string({
-  //   required_error: "Canonical URL is required",
-  // }).url("Canonical URL must be a valid URL"),
-  // ogTitle: z.string().optional(),
-  // ogDescription: z.string().optional(),
-  // ogImage: z
-  //   .string()
-  //   .optional()
-  //   .refine(
-  //     (val) => !val || /^https?:\/\/\S+$/.test(val),
-  //     { message: "OG Image must be a valid URL" }
-  //   ),
 
 });
 
 export const updateSEOZodSchema = z.object({
   metaTitle: z.string()
-    .max(60, "Meta Title should be max 100 characters")
     .optional(),
   metaDescription: z.string()
-    .max(160, "Meta Description should be max 200 characters")
     .optional(),
   metaKeywords: z.string().optional(),
-//   canonicalURL: z.string()
-//     .url("Canonical URL must be a valid URL")
-//     .optional(),
-//   ogTitle: z.string().optional(),
-//   ogDescription: z.string().optional(),
-//  ogImage: z
-//   .string()
-//   .optional()
-//   .refine(
-//     (val) => !val || /^https?:\/\/\S+$/.test(val),
-//     { message: "OG Image must be a valid URL" }
-//   ).optional(),
 });
